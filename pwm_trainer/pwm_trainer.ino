53b41da707ea0368eaff59fc5329aa153dd09b33
Danny Staple <danny@orionrobots.co.uk>
2024-08-01 22:54:13 +0100

    Square waves

--- pwm_trainer/pwm_trainer.ino
+++ pwm_trainer/pwm_trainer.ino
@@ -15,14 +15,14 @@ void setup() {
 }
 
 const int pwm_height = 10;
-const int pwm_width = 30;
+const int pwm_width = 28;
 
 const int left_column = 1;
 const int right_column = 34;
 const int top_row = 1;
 const int bottom_row = 26;
 
-const int text_spacing = 2;
+const int text_spacing = 3;
 const int top_text = top_row + pwm_height + text_spacing;
 const int bottom_text = bottom_row + pwm_height + text_spacing;
 
@@ -35,7 +35,7 @@ void loop() {
   int rPercent = map(rInput, 0, 1024, 0, 100);
   int gPercent = map(gInput, 0, 1024, 0, 100);
   int bPercent = map(bInput, 0, 1024, 0, 100);
-
+  uView.clear(PAGE);
   analogWrite(rLEDPin, 255 - (rInput / 4));
   squareWave(left_column, top_row, pwm_width, pwm_height, rPercent);
   uView.setCursor(left_column, top_text);
@@ -48,6 +48,11 @@ void loop() {
   squareWave(left_column, bottom_row, pwm_width, pwm_height, bPercent);
   uView.setCursor(left_column, bottom_text);
   uView.print(bPercent);
+
+  // grid lines
+  uView.line(0, 24, 64, 24);
+  uView.line(32, 0, 32, 48);
+
   // print out the value you read:
   Serial.print(rInput);
   Serial.print(", ");
@@ -59,5 +64,13 @@ void loop() {
 }
 
 void squareWave(int x, int y, int width, int height, int duty_percent) {
-  uView.rect(x, y, width, height);
+  int falling_edge = map(duty_percent, 0, 100, 0, width);
+  // front line
+  uView.line(x, y, x, y + height);
+  // Top edge
+  uView.line(x, y, x + falling_edge, y);
+  uView.line(x + falling_edge, y, x + falling_edge, y + height);
+  uView.line(x + falling_edge, y + height, x + width, y + height);
+  // end line
+  uView.line(x + width, y, x + width, y + height);
 }
